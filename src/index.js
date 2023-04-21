73c7f81107e23303cb659a2ba5708dcb62cb7e5f
Nataliia Korotych <natalya.cherlenyuk@gmail.com>
2023-04-21 22:21:44 +0200

    up

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './css/styles.css';
 import{fetchCountries} from './service/api';
 import debounce from 'lodash.debounce';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const DEBOUNCE_DELAY = 300;
 
@@ -13,23 +14,40 @@ inputEl.addEventListener('input',debounce(onInputCountry,DEBOUNCE_DELAY))
 
 function onInputCountry(event){
 
-const countryName=event.target.value;
+const countryName=event.target.value.trim();
 
+if(countryName===""){
+    listEl.innerHTML='';
+    containerEl.innerHTML='';
+    return;
+}
    fetchCountries(countryName)
     .then(data=>{
-        if(data.length>=2){
-            listEl.innerHTML=markupalist(data)
+        
+        if(data.length>10){
+            listEl.innerHTML='';
+            Notify.info("Too many matches found. Please enter a more specific name.");
         }
-        else{
-            listEl.innerHTML= ''
-            containerEl.innerHTML=markupContainerEl(data)
+        if (data.length===1){
+            containerEl.innerHTML=markupContainerEl(data);
+            
+
+        }
+        if(data.length>1){
+            containerEl.innerHTML='';
+            
+        }
+        if(data.length>=2 || data.length<10){
+            listEl.innerHTML=markupalist(data);
+            
         }
     })
-    .catch(error=>console.log(error));
+    .catch(clearMarkup);
+  
 
 }
 
-
+ 
 function markupalist (arr){
 return arr.map(({flags:{svg},name:{common}})=>
 `<li class="item">
@@ -52,7 +70,12 @@ return `<div class="country-info">
 <p>Languages: ${Object.values(languages).join(', ')}</p>`}).join('')
  
 }
+function clearMarkup(){
+    listEl.innerHTML= '';
+    containerEl.innerHTML='';
+    Notify.failure('Oops, there is no country with that name')
 
+}
 
 
 
