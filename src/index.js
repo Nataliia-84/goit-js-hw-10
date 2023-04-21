cf66d2d96361bf316d467cb59be5d61666959186
Nataliia Korotych <natalya.cherlenyuk@gmail.com>
2023-04-21 23:23:57 +0200

    up

--- src/index.js
+++ src/index.js
@@ -52,7 +52,7 @@ if(countryName===""){
 function markupalist (arr){
 return arr.map(({flags:{svg},name:{common}})=>
 `<li class="item">
-<img src="${svg}" alt="${common}" width="50">
+<img src="${svg}" alt="${common}" width="50px" height="50px">
 <h2>${common}</h2>
 </li>`).join('')
 }
@@ -63,12 +63,14 @@ function markupContainerEl(arr){
   
  return arr.map(({flags:{svg},name:{official},population,capital,languages}) =>{
     
-return `<div class="country-info">
-<img src="${svg}" alt="${official}" width="150px">
+return `
+<div class="container">
+<img src="${svg}" alt="${official}" width="50px" height="50px">
 <h2>${official}</h2>
-<h3>Capital: ${capital}</h3>
-<p>Population: ${population}</p>
-<p>Languages: ${Object.values(languages).join(', ')}</p>`}).join('')
+</div>
+<h3><b>Capital:</b> ${capital}</h3>
+<p><b>Population:</b> ${population}</p>
+<p><b>Languages:</b> ${Object.values(languages).join(', ')}</p>`}).join('')
  
 }
 function clearMarkup(){
